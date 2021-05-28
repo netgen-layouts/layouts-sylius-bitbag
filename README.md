a7e2768dc8ea352c6524af85f3a80fddfc2c5486
Edi Modrić <edi.modric@gmail.com>
2021-05-28 15:51:35 +0200

    Initial import

---
+++ README.md
@@ -0,0 +1,8 @@
+# Netgen Layouts, Sylius eCommerce & BitBag Sylius CMS plugin integration
+
+## Running tests
+
+Running tests requires that you have complete vendors installed, so run
+`composer install` before running the tests.
+
+You can run unit tests by simply calling `vendor/bin/phpunit` from the repo root.
---
+++ bundle/DependencyInjection/NetgenLayoutsSyliusBitBagExtension.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Bundle\LayoutsSyliusBitBagBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Loader\DelegatingLoader;
+use Symfony\Component\Config\Loader\LoaderResolver;
+use Symfony\Component\Config\Resource\FileResource;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader\GlobFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Yaml\Yaml;
+use function file_get_contents;
+
+final class NetgenLayoutsSyliusBitBagExtension extends Extension implements PrependExtensionInterface
+{
+    /**
+     * @param mixed[] $configs
+     */
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $locator = new FileLocator(__DIR__ . '/../Resources/config');
+
+        $loader = new DelegatingLoader(
+            new LoaderResolver(
+                [
+                    new GlobFileLoader($container, $locator),
+                    new YamlFileLoader($container, $locator),
+                ],
+            ),
+        );
+
+        $loader->load('services/**/*.yaml', 'glob');
+        $loader->load('default_settings.yaml');
+    }
+
+    public function prepend(ContainerBuilder $container): void
+    {
+        $prependConfigs = [
+            'value_types.yaml' => 'netgen_layouts',
+        ];
+
+        foreach ($prependConfigs as $configFile => $prependConfig) {
+            $configFile = __DIR__ . '/../Resources/config/' . $configFile;
+            $config = Yaml::parse((string) file_get_contents($configFile));
+            $container->prependExtensionConfig($prependConfig, $config);
+            $container->addResource(new FileResource($configFile));
+        }
+    }
+}
