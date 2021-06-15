2e060404210c023f354892a0a0d19dd2445798f6
Randy Čupić <randy@netgen.hr>
2021-06-15 09:21:55 +0200

    LAYOUTS-615 Create validator for section

---
+++ lib/Validator/Constraint/Section.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Layouts\Sylius\BitBag\Validator\Constraint;
+
+use Symfony\Component\Validator\Constraint;
+
+class Section extends Constraint
+{
+    public string $message = 'netgen_layouts.sylius.bitbag.section.section_not_found';
+
+    public function validatedBy(): string
+    {
+        return 'nglayouts_sylius_bitbag_section';
+    }
+}
---
+++ lib/Validator/SectionValidator.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Layouts\Sylius\BitBag\Validator;
+
+use BitBag\SyliusCmsPlugin\Entity\SectionInterface;
+use Netgen\Layouts\Sylius\BitBag\Repository\SectionRepositoryInterface;
+use Netgen\Layouts\Sylius\BitBag\Validator\Constraint\Section;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+use function is_scalar;
+
+final class SectionValidator extends ConstraintValidator
+{
+    private SectionRepositoryInterface $sectionRepository;
+
+    public function __construct(SectionRepositoryInterface $sectionRepository)
+    {
+        $this->sectionRepository = $sectionRepository;
+    }
+
+    public function validate($value, Constraint $constraint): void
+    {
+        if ($value === null) {
+            return;
+        }
+
+        if (!$constraint instanceof Section) {
+            throw new UnexpectedTypeException($constraint, Section::class);
+        }
+
+        if (!is_scalar($value)) {
+            throw new UnexpectedTypeException($value, 'scalar');
+        }
+
+        $section = $this->sectionRepository->find($value);
+        if (!$section instanceof SectionInterface) {
+            $this->context->buildViolation($constraint->message)
+                ->setParameter('%sectionId%', (string) $value)
+                ->addViolation();
+        }
+    }
+}
