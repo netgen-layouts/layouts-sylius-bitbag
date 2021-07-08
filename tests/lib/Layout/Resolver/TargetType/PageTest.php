ece1825734915955aae845e8513b6a7da9753ca1
Randy Čupić <randy@netgen.hr>
2021-07-08 10:49:47 +0200

    LAYOUTS-614 LAYOUTS-615 Add missing tests for target types

--- tests/lib/Layout/Resolver/TargetType/PageTest.php
+++ tests/lib/Layout/Resolver/TargetType/PageTest.php
@@ -6,8 +6,12 @@ namespace Netgen\Layouts\Sylius\BitBag\Tests\Layout\Resolver\TargetType;
 
 use BitBag\SyliusCmsPlugin\Repository\PageRepositoryInterface;
 use Netgen\Layouts\Sylius\BitBag\Layout\Resolver\TargetType\Page;
+use Netgen\Layouts\Sylius\BitBag\Tests\Stubs\Page as PageStub;
+use Netgen\Layouts\Sylius\BitBag\Tests\Validator\RepositoryValidatorFactory;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Validation;
 
 final class PageTest extends TestCase
 {
@@ -31,5 +35,66 @@ final class PageTest extends TestCase
     public function testGetType(): void
     {
         self::assertSame('bitbag_page', $this->targetType::getType());
+    }
+
+    /**
+     * @covers \Netgen\Layouts\Sylius\BitBag\Layout\Resolver\TargetType\Page::getConstraints
+     */
+    public function testValidationValid(): void
+    {
+        $this->repositoryMock
+            ->expects(self::once())
+            ->method('find')
+            ->with(self::identicalTo(42))
+            ->willReturn(new PageStub(42, 'contact-us'));
+
+        $validator = Validation::createValidatorBuilder()
+            ->setConstraintValidatorFactory(new RepositoryValidatorFactory($this->repositoryMock))
+            ->getValidator();
+
+        $errors = $validator->validate(42, $this->targetType->getConstraints());
+        self::assertCount(0, $errors);
+    }
+
+    /**
+     * @covers \Netgen\Layouts\Sylius\BitBag\Layout\Resolver\TargetType\Page::getConstraints
+     */
+    public function testValidationInvalid(): void
+    {
+        $this->repositoryMock
+            ->expects(self::once())
+            ->method('find')
+            ->with(self::identicalTo(42))
+            ->willReturn(null);
+
+        $validator = Validation::createValidatorBuilder()
+            ->setConstraintValidatorFactory(new RepositoryValidatorFactory($this->repositoryMock))
+            ->getValidator();
+
+        $errors = $validator->validate(42, $this->targetType->getConstraints());
+        self::assertNotCount(0, $errors);
+    }
+
+    /**
+     * @covers \Netgen\Layouts\Sylius\BitBag\Layout\Resolver\TargetType\Page::provideValue
+     */
+    public function testProvideValue(): void
+    {
+        $page = new PageStub(42, 'contact-us');
+
+        $request = Request::create('/');
+        $request->attributes->set('nglayouts_sylius_bitbag_page', $page);
+
+        self::assertSame(42, $this->targetType->provideValue($request));
+    }
+
+    /**
+     * @covers \Netgen\Layouts\Sylius\BitBag\Layout\Resolver\TargetType\Page::provideValue
+     */
+    public function testProvideValueWithNoPage(): void
+    {
+        $request = Request::create('/');
+
+        self::assertNull($this->targetType->provideValue($request));
     }
 }
--- tests/lib/Layout/Resolver/TargetType/SectionTest.php
+++ tests/lib/Layout/Resolver/TargetType/SectionTest.php
@@ -6,8 +6,12 @@ namespace Netgen\Layouts\Sylius\BitBag\Tests\Layout\Resolver\TargetType;
 
 use BitBag\SyliusCmsPlugin\Repository\SectionRepositoryInterface;
 use Netgen\Layouts\Sylius\BitBag\Layout\Resolver\TargetType\Section;
+use Netgen\Layouts\Sylius\BitBag\Tests\Stubs\Section as SectionStub;
+use Netgen\Layouts\Sylius\BitBag\Tests\Validator\RepositoryValidatorFactory;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Validation;
 
 final class SectionTest extends TestCase
 {
@@ -31,5 +35,66 @@ final class SectionTest extends TestCase
     public function testGetType(): void
     {
         self::assertSame('bitbag_section', $this->targetType::getType());
+    }
+
+    /**
+     * @covers \Netgen\Layouts\Sylius\BitBag\Layout\Resolver\TargetType\Section::getConstraints
+     */
+    public function testValidationValid(): void
+    {
+        $this->repositoryMock
+            ->expects(self::once())
+            ->method('find')
+            ->with(self::identicalTo(42))
+            ->willReturn(new SectionStub(42, 'blog'));
+
+        $validator = Validation::createValidatorBuilder()
+            ->setConstraintValidatorFactory(new RepositoryValidatorFactory($this->repositoryMock))
+            ->getValidator();
+
+        $errors = $validator->validate(42, $this->targetType->getConstraints());
+        self::assertCount(0, $errors);
+    }
+
+    /**
+     * @covers \Netgen\Layouts\Sylius\BitBag\Layout\Resolver\TargetType\Section::getConstraints
+     */
+    public function testValidationInvalid(): void
+    {
+        $this->repositoryMock
+            ->expects(self::once())
+            ->method('find')
+            ->with(self::identicalTo(42))
+            ->willReturn(null);
+
+        $validator = Validation::createValidatorBuilder()
+            ->setConstraintValidatorFactory(new RepositoryValidatorFactory($this->repositoryMock))
+            ->getValidator();
+
+        $errors = $validator->validate(42, $this->targetType->getConstraints());
+        self::assertNotCount(0, $errors);
+    }
+
+    /**
+     * @covers \Netgen\Layouts\Sylius\BitBag\Layout\Resolver\TargetType\Section::provideValue
+     */
+    public function testProvideValue(): void
+    {
+        $section = new SectionStub(42, 'blog');
+
+        $request = Request::create('/');
+        $request->attributes->set('nglayouts_sylius_bitbag_section', $section);
+
+        self::assertSame(42, $this->targetType->provideValue($request));
+    }
+
+    /**
+     * @covers \Netgen\Layouts\Sylius\BitBag\Layout\Resolver\TargetType\Section::provideValue
+     */
+    public function testProvideValueWithNoSection(): void
+    {
+        $request = Request::create('/');
+
+        self::assertNull($this->targetType->provideValue($request));
     }
 }
