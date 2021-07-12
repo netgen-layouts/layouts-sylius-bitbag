32c070fc75479e5f3282a65145e714873d8ecc1b
Randy Čupić <randy@netgen.hr>
2021-07-12 13:17:16 +0200

    LAYOUTS-621 Add tests for custom layouts block for BitBag entity field

---
+++ tests/lib/Block/BlockDefinition/Handler/EntityFieldHandlerTest.php
@@ -0,0 +1,142 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Layouts\Sylius\BitBag\Tests\Block\BlockDefinition\Handler;
+
+use Netgen\Layouts\API\Values\Block\Block;
+use Netgen\Layouts\Block\DynamicParameters;
+use Netgen\Layouts\Parameters\Parameter;
+use Netgen\Layouts\Sylius\BitBag\Block\BlockDefinition\Handler\BitBagEntityField;
+use Netgen\Layouts\Sylius\BitBag\Block\BlockDefinition\Handler\EntityFieldHandler;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Netgen\Layouts\Sylius\BitBag\Tests\Stubs\Page as PageStub;
+use Netgen\Layouts\Sylius\BitBag\Tests\Stubs\Section as SectionStub;
+
+final class EntityFieldHandlerTest extends TestCase
+{
+    private MockObject $requestStackMock;
+
+    private EntityFieldHandler $handler;
+
+    protected function setUp(): void
+    {
+        $this->requestStackMock = $this->createMock(RequestStack::class);
+
+        $this->handler = new EntityFieldHandler($this->requestStackMock);
+    }
+
+    /**
+     * @covers \Netgen\Layouts\Sylius\BitBag\Block\BlockDefinition\Handler\EntityFieldHandler::isContextual
+     */
+    public function testIsContextual(): void
+    {
+        self::assertTrue($this->handler->isContextual(new Block()));
+    }
+
+    /**
+     * @covers \Netgen\Layouts\Sylius\BitBag\Block\BlockDefinition\Handler\EntityFieldHandler::__construct
+     * @covers \Netgen\Layouts\Sylius\BitBag\Block\BlockDefinition\Handler\EntityFieldHandler::getDynamicParameters
+     * @covers \Netgen\Layouts\Sylius\BitBag\Block\BlockDefinition\Handler\EntityFieldHandler::getCurrentBitBagEntity
+     */
+    public function testGetDynamicParametersWithPage(): void
+    {
+        $page = new PageStub(5, 'about-us');
+        $page->setCurrentLocale('nor_NO');
+        $page->setName('About us');
+
+        $request = Request::create('/');
+        $request->attributes->set('nglayouts_sylius_bitbag_page', $page);
+
+        $this->requestStackMock
+            ->expects(self::once())
+            ->method('getCurrentRequest')
+            ->willReturn($request);
+
+        $params = new DynamicParameters();
+
+        $this->handler->getDynamicParameters($params, Block::fromArray([
+            'parameters' => [
+                'field_identifier' => Parameter::fromArray([
+                    'name' => 'field_identifier',
+                    'value' => 'name',
+                ])
+            ]
+        ]));
+
+        self::assertArrayHasKey('field', $params);
+
+        $field = BitBagEntityField::fromBitBagEntity($page, 'name');
+
+        self::assertSame($field->isEmpty(), $params['field']->isEmpty());
+        self::assertSame($field->getType(), $params['field']->getType());
+        self::assertSame($field->getValue(), $params['field']->getValue());
+    }
+
+    /**
+     * @covers \Netgen\Layouts\Sylius\BitBag\Block\BlockDefinition\Handler\EntityFieldHandler::__construct
+     * @covers \Netgen\Layouts\Sylius\BitBag\Block\BlockDefinition\Handler\EntityFieldHandler::getDynamicParameters
+     * @covers \Netgen\Layouts\Sylius\BitBag\Block\BlockDefinition\Handler\EntityFieldHandler::getCurrentBitBagEntity
+     */
+    public function testGetDynamicParametersWithSection(): void
+    {
+        $section = new SectionStub(5, 'blog');
+
+        $request = Request::create('/');
+        $request->attributes->set('nglayouts_sylius_bitbag_section', $section);
+
+        $this->requestStackMock
+            ->expects(self::once())
+            ->method('getCurrentRequest')
+            ->willReturn($request);
+
+        $params = new DynamicParameters();
+
+        $this->handler->getDynamicParameters($params, Block::fromArray([
+            'parameters' => [
+                'field_identifier' => Parameter::fromArray([
+                    'name' => 'field_identifier',
+                    'value' => 'code',
+                ])
+            ]
+        ]));
+
+        self::assertArrayHasKey('field', $params);
+
+        $field = BitBagEntityField::fromBitBagEntity($section, 'code');
+
+        self::assertSame($field->isEmpty(), $params['field']->isEmpty());
+        self::assertSame($field->getType(), $params['field']->getType());
+        self::assertSame($field->getValue(), $params['field']->getValue());
+    }
+
+    /**
+     * @covers \Netgen\Layouts\Sylius\BitBag\Block\BlockDefinition\Handler\EntityFieldHandler::__construct
+     * @covers \Netgen\Layouts\Sylius\BitBag\Block\BlockDefinition\Handler\EntityFieldHandler::getDynamicParameters
+     * @covers \Netgen\Layouts\Sylius\BitBag\Block\BlockDefinition\Handler\EntityFieldHandler::getCurrentBitBagEntity
+     */
+    public function testGetDynamicParametersWithoutRequest(): void
+    {
+        $this->requestStackMock
+            ->expects(self::once())
+            ->method('getCurrentRequest')
+            ->willReturn(null);
+
+        $params = new DynamicParameters();
+
+        $this->handler->getDynamicParameters($params, Block::fromArray([
+            'parameters' => [
+                'field_identifier' => Parameter::fromArray([
+                    'name' => 'field_identifier',
+                    'value' => 'code',
+                ])
+            ]
+        ]));
+
+        self::assertArrayHasKey('field', $params);
+        self::assertNull($params['field']);
+    }
+}
