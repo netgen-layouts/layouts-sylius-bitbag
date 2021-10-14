305aec44bbdc17038b3613b06f30e69492629ab1
Randy Čupić <randy@netgen.hr>
2021-10-14 08:31:40 +0200

    NGSTACK-528 Create new value type for FAQ

---
+++ lib/Item/ValueUrlGenerator/FrequentlyAskedQuestionValueUrlGenerator.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Layouts\Sylius\BitBag\Item\ValueUrlGenerator;
+
+use Netgen\Layouts\Item\ValueUrlGeneratorInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+/**
+ * @implements \Netgen\Layouts\Item\ValueUrlGeneratorInterface<\BitBag\SyliusCmsPlugin\Entity\FrequentlyAskedQuestionInterface>
+ */
+final class FrequentlyAskedQuestionValueUrlGenerator implements ValueUrlGeneratorInterface
+{
+    private UrlGeneratorInterface $urlGenerator;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator)
+    {
+        $this->urlGenerator = $urlGenerator;
+    }
+
+    public function generate(object $object): ?string
+    {
+        return $this->urlGenerator->generate(
+            'bitbag_sylius_cms_plugin_admin_frequently_asked_question_update',
+            [
+                'id' => $object->getId(),
+            ],
+        );
+    }
+}
