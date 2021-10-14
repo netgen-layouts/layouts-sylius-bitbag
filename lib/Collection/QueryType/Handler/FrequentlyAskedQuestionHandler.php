bbeb486ff3932c1bb32bb412da9e82713aa0465e
Randy Čupić <randy@netgen.hr>
2021-10-14 08:38:36 +0200

    NGSTACK-528 Create query type for FAQ

---
+++ lib/Collection/QueryType/Handler/FrequentlyAskedQuestionHandler.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Layouts\Sylius\BitBag\Collection\QueryType\Handler;
+
+use Netgen\Layouts\API\Values\Collection\Query;
+use Netgen\Layouts\Collection\QueryType\QueryTypeHandlerInterface;
+use Netgen\Layouts\Parameters\ParameterBuilderInterface;
+use Netgen\Layouts\Sylius\BitBag\Collection\QueryType\Handler\Traits\BitBagSortingTrait;
+use Netgen\Layouts\Sylius\BitBag\Collection\QueryType\Handler\Traits\SyliusChannelFilterTrait;
+use Netgen\Layouts\Sylius\BitBag\Repository\FrequentlyAskedQuestionRepositoryInterface;
+use Sylius\Component\Locale\Context\LocaleContextInterface;
+use const PHP_INT_MAX;
+
+final class FrequentlyAskedQuestionHandler implements QueryTypeHandlerInterface
+{
+    use BitBagSortingTrait;
+    use SyliusChannelFilterTrait;
+
+    private FrequentlyAskedQuestionRepositoryInterface $frequentlyAskedQuestionRepository;
+
+    private LocaleContextInterface $localeContext;
+
+    /** @var array<string, string> */
+    private array $sortingOptions = [
+        'Position' => 'position',
+        'Question' => 'translations.question',
+        'Answer' => 'translations.answer',
+        'Code' => 'code',
+    ];
+
+    public function __construct(
+        FrequentlyAskedQuestionRepositoryInterface $frequentlyAskedQuestionRepository,
+        LocaleContextInterface $localeContext
+    ) {
+        $this->frequentlyAskedQuestionRepository = $frequentlyAskedQuestionRepository;
+        $this->localeContext = $localeContext;
+    }
+
+    public function buildParameters(ParameterBuilderInterface $builder): void
+    {
+        $this->buildSyliusChannelFilterParameters($builder);
+        $this->buildBitBagSortingParameters($builder, $this->sortingOptions);
+    }
+
+    public function getValues(Query $query, int $offset = 0, ?int $limit = null): iterable
+    {
+        $queryBuilder = $this->frequentlyAskedQuestionRepository->createListQueryBuilder(
+            $this->localeContext->getLocaleCode(),
+        );
+
+        $this->addSyliusChannelFilterCriterion($query, $queryBuilder);
+        $this->addBitBagSortingClause($query, $queryBuilder);
+
+        $paginator = $this->frequentlyAskedQuestionRepository->createFilterPaginator($queryBuilder);
+
+        $limit = $limit ?? PHP_INT_MAX;
+
+        $paginator->setMaxPerPage($limit);
+        $paginator->setCurrentPage((int) ($offset / $limit) + 1);
+
+        return $paginator->getCurrentPageResults();
+    }
+
+    public function getCount(Query $query): int
+    {
+        $queryBuilder = $this->frequentlyAskedQuestionRepository->createListQueryBuilder(
+            $this->localeContext->getLocaleCode(),
+        );
+
+        $this->addSyliusChannelFilterCriterion($query, $queryBuilder);
+
+        $paginator = $this->frequentlyAskedQuestionRepository->createFilterPaginator($queryBuilder);
+
+        return $paginator->getNbResults();
+    }
+
+    public function isContextual(Query $query): bool
+    {
+        return false;
+    }
+}
