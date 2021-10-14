d69a9b8fc249b6b872629fa02e76302cefadf52a
Randy Čupić <randy@netgen.hr>
2021-10-14 08:38:22 +0200

    NGSTACK-528 Create tests for content browser for FAQ

---
+++ tests/lib/Item/ColumnProvider/FrequentlyAskedQuestion/CodeTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Layouts\Sylius\BitBag\Tests\Item\ColumnProvider\FrequentlyAskedQuestion;
+
+use Netgen\Layouts\Sylius\BitBag\ContentBrowser\Item\FrequentlyAskedQuestion\Item as FrequentlyAskedQuestionItem;
+use Netgen\Layouts\Sylius\BitBag\Item\ColumnProvider\FrequentlyAskedQuestion\Code;
+use Netgen\Layouts\Sylius\BitBag\Tests\Item\Stubs\FrequentlyAskedQuestion as FrequentlyAskedQuestionStub;
+use PHPUnit\Framework\TestCase;
+
+final class CodeTest extends TestCase
+{
+    private Code $codeColumn;
+
+    protected function setUp(): void
+    {
+        $this->codeColumn = new Code();
+    }
+
+    /**
+     * @covers \Netgen\Layouts\Sylius\BitBag\Item\ColumnProvider\FrequentlyAskedQuestion\Code::getValue
+     */
+    public function testGetValue(): void
+    {
+        $frequentlyAskedQuestion = new FrequentlyAskedQuestionStub(5, 'TEST_QUESTION');
+        $item = new FrequentlyAskedQuestionItem($frequentlyAskedQuestion);
+
+        self::assertSame('TEST_QUESTION', $this->codeColumn->getValue($item));
+    }
+}
