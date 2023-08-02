5f40185cd077b581c6191441b6bfe6051ea8093e
Randy Čupić <randy@netgen.hr>
2023-08-02 18:52:26 +0200

    Create interfaces for components and their translations

---
+++ lib/API/ComponentInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Layouts\Sylius\BitBag\API;
+
+use Sylius\Component\Resource\Model\ResourceInterface;
+use Sylius\Component\Resource\Model\TimestampableInterface;
+use Sylius\Component\Resource\Model\ToggleableInterface;
+use Sylius\Component\Resource\Model\TranslatableInterface;
+
+interface ComponentInterface extends ResourceInterface, ToggleableInterface, TimestampableInterface, TranslatableInterface
+{
+    public static function getIdentifier(): string;
+
+    public function getName(): string;
+}
