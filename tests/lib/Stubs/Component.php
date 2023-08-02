01a4fb17482d8e80b399e04de56bd3cf12858938
Randy Čupić <randy@netgen.hr>
2023-08-02 22:11:54 +0200

    Add content browser integration for component

--- tests/lib/Stubs/Component.php
+++ tests/lib/Stubs/Component.php
@@ -11,12 +11,18 @@ final class Component extends AbstractComponent
 {
     private string $name;
 
-    public function __construct(int $id, string $name)
-    {
+    public function __construct(
+        int $id,
+        string $name,
+        bool $enabled = true,
+        string $currentLocale = 'en',
+    ) {
         parent::__construct();
 
         $this->id = $id;
         $this->name = $name;
+        $this->enabled = $enabled;
+        $this->currentLocale = $currentLocale;
     }
 
     public static function getIdentifier(): string
