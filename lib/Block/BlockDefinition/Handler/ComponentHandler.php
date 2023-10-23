108c931d2a919c5f0a062eaa9f97323cc1ffb90d
Randy Čupić <randy@netgen.hr>
2023-10-23 12:52:27 +0200

    Add layouts block for components

---
+++ lib/Block/BlockDefinition/Handler/ComponentHandler.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Layouts\Sylius\BitBag\Block\BlockDefinition\Handler;
+
+use Netgen\Layouts\Block\BlockDefinition\BlockDefinitionHandler;
+use Netgen\Layouts\Parameters\ParameterBuilderInterface;
+use Netgen\Layouts\Parameters\ParameterType;
+use Netgen\Layouts\Sylius\BitBag\Parameters\ParameterType\ComponentType;
+
+final class ComponentHandler extends BlockDefinitionHandler
+{
+    public function buildParameters(ParameterBuilderInterface $builder): void
+    {
+        $builder->add(
+            'component_type_identifier',
+            ParameterType\HiddenType::class,
+            [
+                'required' => true,
+                'readonly' => true,
+            ],
+        );
+
+        $builder->add(
+            'component',
+            ComponentType::class,
+            [
+                'component_type_identifier' => 'bitbag_component',
+                'label' => 'block.bitbag_component.component',
+            ],
+        );
+    }
+}
